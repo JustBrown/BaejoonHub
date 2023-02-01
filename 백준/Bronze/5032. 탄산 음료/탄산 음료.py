bd2c2aca5c97c54dffeddf71faa13f20c761a69d
JustBrown <110770392+JustBrown@users.noreply.github.com>
2023-02-02 03:27:16 +0900

    [Bronze II] Title: 탄산 음료, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5032. 탄산 음료/탄산 음료.py
+++ 백준/Bronze/5032. 탄산 음료/탄산 음료.py
@@ -1,5 +1,4 @@
 e, f, c = map(int, input().split())
 result, empty_b = 0, e+f
-while empty_b//c>0:
-    result, empty_b = result+empty_b//c, empty_b%c+empty_b//c
+while empty_b//c>0: result, empty_b = result+empty_b//c, empty_b%c+empty_b//c
 print(result)
