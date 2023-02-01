8687a34e86e2b71316fbf3cc664ac5bb73669495
JustBrown <110770392+JustBrown@users.noreply.github.com>
2023-02-02 03:48:48 +0900

    [Bronze II] Title: 2017 아주대학교 프로그래밍 경시대회 (Large), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14593. 2017 아주대학교 프로그래밍 경시대회 （Large）/2017 아주대학교 프로그래밍 경시대회 （Large）.py
+++ 백준/Bronze/14593. 2017 아주대학교 프로그래밍 경시대회 （Large）/2017 아주대학교 프로그래밍 경시대회 （Large）.py
@@ -1,4 +1,4 @@
-import sys
-input = sys.stdin.readline
-array = [tuple(map(int, input().split())) for _ in range(int(input()))]
-print(array.index(sorted(array, key=lambda x:(-x[0], x[1], x[2]))[0])+1)
+n = int(input())
+array = [tuple(map(int, input().split())) for _ in range(n)]
+sorted_arr = sorted(array, key=lambda x:(-x[0], x[1], x[2]))
+print(array.index(sorted_arr[0])+1)
