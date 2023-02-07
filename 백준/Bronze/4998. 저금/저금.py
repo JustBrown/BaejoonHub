37a61e13ef9e428e0a8aa65b26ab0616e3586eb2
JustBrown <110770392+JustBrown@users.noreply.github.com>
2023-02-07 15:57:52 +0900

    [Bronze II] Title: 저금, Time: 52 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/4998. 저금/저금.py
+++ 백준/Bronze/4998. 저금/저금.py
@@ -1,11 +1,9 @@
+from math import log10
+
 while True:
     try:
         input_data = input()
     except EOFError:
         break
     n, b, m = map(float, input_data.split())
-    cnt = 0
-    while n<=m:
-        n *= (1+0.01*b)
-        cnt += 1
-    print(cnt)
+    print(int((log10(m)-log10(n))/log10(1+0.01*b))+1)
