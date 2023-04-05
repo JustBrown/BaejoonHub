4135556f749dcdac5b301977b842bac6498be7cf
JustBrown <110770392+JustBrown@users.noreply.github.com>
2023-04-05 18:56:38 +0900

    [Bronze III] Title: IBM 빼기 1, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/6321. IBM 빼기 1/IBM 빼기 1.py
+++ 백준/Bronze/6321. IBM 빼기 1/IBM 빼기 1.py
@@ -4,4 +4,4 @@ input = sys.stdin.readline
 for i in range(int(input())):
     name = input()[:-1]
     print(f'String #{i+1}')
-    print(''.join([chr(ord(n)+1) if ord(n)!=90 else 'A' for n in name]) + '\n')
+    print(''.join([chr(ord(n)+1) if n!='Z' else 'A' for n in name]) + '\n')
