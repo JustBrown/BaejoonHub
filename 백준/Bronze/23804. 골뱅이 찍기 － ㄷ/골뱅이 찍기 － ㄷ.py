7b493c91d954999d7a88120fa7d1db3b6de13aae
JustBrown <110770392+JustBrown@users.noreply.github.com>
2023-04-17 21:25:07 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.py
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.py
@@ -1,7 +1,2 @@
-n = int(input())
-
-for i in range(5):
-    if i==0 or i==4:
-        [print('@'*n*5) for _ in range(n)]
-    else:
-        [print('@'*n) for _ in range(n)]
+n = int(input()) 
+[[print('@'*n*5 if i==0 or i==4 else '@'*n) for _ in range(n)] for i in range(5)]
