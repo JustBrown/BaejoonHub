35564d720dcf5af642459116d25a9f8775312436
JustBrown <110770392+JustBrown@users.noreply.github.com>
2023-05-11 02:18:06 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
@@ -1,2 +1,3 @@
 n = int(input())
-[print(' '*(n-i)+'*'*i if i<=n else ' '*(i-n)+'*'*(2*n-i)) for i in range(1, 2*n)]
+for i in range(1, 2*n):
+    print(' '*(n-i)+'*'*i if i<=n else ' '*(i-n)+'*'*(2*n-i))
