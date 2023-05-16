b70191671d27cc88224a395bae88321cddeeb10e
JustBrown <110770392+JustBrown@users.noreply.github.com>
2023-05-16 23:41:08 +0900

    [Bronze II] Title: 완전제곱수, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.py
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.py
@@ -3,8 +3,12 @@ from math import ceil
 m = int(input())
 n = int(input())
 
-PSM = []
+sum_sqr, min_sqr = 0, 0
 for i in range(ceil(m**.5), int(n**.5)+1):
-    PSM.append(i**2)
+    sqr = i**2
+    if min_sqr == 0:
+        min_sqr = sqr
+    sum_sqr += sqr
+    
 
-print(-1) if len(PSM)==0 else print(sum(PSM), PSM[0])
+print(-1) if sum_sqr==0 else print(sum_sqr, min_sqr, sep='\n')
