36463bb0ae98c0154ad3747d3db9f6e3e74de2a5
JustBrown <110770392+JustBrown@users.noreply.github.com>
2023-05-16 23:38:13 +0900

    [Bronze II] Title: 완전제곱수, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.py
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.py
@@ -1,26 +1,10 @@
-def isPSM(num):
-    factors = []
-    # 소인수분해
-    d = 2
-    while num >= d:
-        if num % d == 0:
-            factors.append(d)
-            num //= d
-        else:
-            d += 1
-    
-    # 완전제곱수 여부 확인
-    for i in set(factors):
-        if factors.count(i)%2!=0:
-            return False
-    return True
+from math import ceil
 
 m = int(input())
 n = int(input())
 
 PSM = []
-for i in range(m, n+1):
-    if isPSM(i):
-        PSM.append(i)
+for i in range(ceil(m**.5), int(n**.5)+1):
+    PSM.append(i**2)
 
-print(-1) if len(PSM) == 0 else print(sum(PSM), PSM[0], sep='\n')
+print(-1) if len(PSM)==0 else print(sum(PSM), PSM[0])
